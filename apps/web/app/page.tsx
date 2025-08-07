6f871f251920d0b5201ae2ed3b3442545b8da7f3
Aura Cho <acho1833@inst.hcpss.org>
2025-08-07 20:30:18 +0000

    init

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,18 @@
-import { Button } from "@workspace/ui/components/button";
-import { add } from "@workspace/math/add";
+'use client';
+
+import { useMutation, useQuery } from 'convex/react';
+import { api } from '@workspace/backend/_generated/api';
+import { Button } from '@workspace/ui/components/button';
 
 export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello web apps!</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2, 3)}</p>
-      </div>
-    </div>
-  );
+    const users = useQuery(api.users.getMany);
+    const addUser = useMutation(api.users.add);
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-svh">
+            <p>apps/web</p>
+            <Button onClick={() => addUser()}   >Add</Button>
+            <pre>{JSON.stringify(users, null, 2)}</pre>
+        </div>
+    );
 }
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,15 @@
-import { Button } from "@workspace/ui/components/button"
-import { Input } from "@workspace/ui/components/input"
+'use client';
+
+import { useQuery } from 'convex/react';
+import { api } from '@workspace/backend/_generated/api';
 
 export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello widgets!</h1>
-        <Button size="sm">Button</Button>
-        <Input />
-      </div>
-    </div>
-  )
+    const users = useQuery(api.users.getMany);
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-svh">
+            <p>apps/web</p>
+            <pre>{JSON.stringify(users, null, 2)}</pre>
+        </div>
+    );
 }
